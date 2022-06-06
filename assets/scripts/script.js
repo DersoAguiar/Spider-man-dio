94673d0565831352e6871deafaff204cdea5c676
DersoAguiar <andersonaguiar.m.z@gmail.com>
2022-06-06 13:32:12 -0300

    Porjeto Finalizado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,7 +10,7 @@ function handleMouseLeave() {
 
 function addEventListenersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
-  
+
   for (let index = 0; index < cardElements.length; index++) {
     const card = cardElements[index];
     card.addEventListener('mouseenter', handleMouseEnter);
@@ -22,12 +22,12 @@ document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
 function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
-  const carousel = document.querySelector('.s-cards-carousel');
+  const carousel = document.querySelector('.s-card-carousel');
   const transform = carousel.style.transform;
   const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
   const rotateYDeg = -120 * (Number(selectedItem) - 1);
   const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
-
+  
   carousel.style.transform = newTransform;
 
   const activeButtonElement = document.querySelector('.s-controller__button--active');
